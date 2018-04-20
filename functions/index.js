b6ed5b33dae76a7a9c33c74ce8a90dccb005eadb
Vinicius Lima <viniciuslima76@outlook.com.br>
2018-04-19 23:38:19 -0300

    Adicionado Middleware com Cors

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,12 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const animes = require('./data/animes');
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
+//enable for all origins
+app.use(cors);
 admin.initializeApp(functions.config().firebase);
 
 exports.createNewDatabase = functions.https.onRequest((req, res) => {
@@ -16,15 +21,19 @@ exports.createNewDatabase = functions.https.onRequest((req, res) => {
     res.redirect(303, dataBase.ref());
 });
 
+exports.pushWinner = functions.https.onRequest((req, res) => {
+    const dataBase = admin.database();
+
+    const original = req.query.text;
+
+    dataBase.ref('/winners').push({ nome: original});
+
+    res.sendStatus(202);
+});
+
 exports.getAllAnime = functions.https.onRequest((req, res) => {
     const dataBase = admin.database();
     var animeList = [];
-
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
-    res.setHeader('Access-Control-Allow-Credentials', true); // If needed);
-
 
     dataBase.ref('/animes').on("value", function (snapshot) {
         snapshot.forEach((anime) => { animeList.push(anime) });
@@ -33,10 +42,3 @@ exports.getAllAnime = functions.https.onRequest((req, res) => {
     });
     
 });
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
